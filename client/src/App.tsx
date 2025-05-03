7d1c5919ef1bd317ca33510f0cc5704045f07b8d
Steve <stevedsimkins@gmail.com>
2025-05-03 15:14:12 -0400

    feat: Added tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import beaver from './assets/beaver.svg'
 import { ApiResponse } from 'shared'
-import './App.css'
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
 
@@ -19,44 +18,46 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <a href="https://github.com/stevedylandev/bhvr" target="_blank">
-          <img src={beaver} className="logo" alt="beaver logo" />
-        </a>
-      </div>
-      <h1>bhvr</h1>
-      <h2>Bun + Hono + Vite + React</h2>
+    <div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
+      <a href="https://github.com/stevedylandev/bhvr" target="_blank">
+        <img
+          src={beaver}
+          className="w-16 h-16 cursor-pointer"
+          alt="beaver logo"
+        />
+      </a>
+      <h1 className="text-5xl font-black">bhvr</h1>
+      <h2 className="text-2xl font-bold">Bun + Hono + Vite + React</h2>
       <p>A typesafe fullstack monorepo</p>
-      <div className="card">
-        <button onClick={sendRequest}>
+        <button
+          onClick={sendRequest}
+          className="bg-black text-white px-2.5 py-1.5 rounded-md"
+        >
           Call API
         </button>
         {data && (
-          <pre className='response'>
+          <pre className="bg-gray-100 p-4 rounded-md">
             <code>
             Message: {data.message} <br />
             Success: {data.success.toString()}
             </code>
           </pre>
         )}
-        <pre className='code'>
+        <pre className="bg-gray-100 p-4 rounded-md">
           <code>
-{`
-  .
-  ├── client/               # React frontend
-  ├── server/               # Hono backend
-  ├── shared/               # Shared TypeScript definitions
-  │   └── src/types/        # Type definitions used by both client and server
-  └── package.json          # Root package.json with workspaces
+{`.
+├── client/               # React frontend
+├── server/               # Hono backend
+├── shared/               # Shared TypeScript definitions
+│   └── src/types/        # Type definitions used by both client and server
+└── package.json          # Root package.json with workspaces
 `}
           </code>
         </pre>
-      </div>
-      <p className="read-the-docs">
+      <p className="text-gray-600">
         Click the beaver to learn more
       </p>
-    </>
+    </div>
   )
 }
 
