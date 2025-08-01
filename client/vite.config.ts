e5dfef43af8c9ee6fd5c6e4af2fa40d6d2a155d2
Steve Simkins <stevedsimkins@gmail.com>
2025-07-31 23:56:24 -0400

    chore: Updated vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@client": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server/src"),
-      "@shared": path.resolve(__dirname, "../shared/src"),
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+	plugins: [react(), tailwindcss()],
+});
